e189fe579ce994f9eeaf3690f8fc465529549398
Rikk <tarikmoussa90@gmial.com>
2026-03-25 16:58:54 -0300

    Finalização.

--- sistema_prioridade.py
+++ sistema_prioridade.py
@@ -4,50 +4,17 @@
 #Giovanne Dias - RM: 569750
 
 #Entrada de dados.
-#Severidade
-try:
-    severidade_do_problema = int(input("Dígite a severidade do problema (1 a 3): "))
-except ValueError:
-    print("Erro: você deve digitar um número!!")
-if severidade_do_problema < 1 or severidade_do_problema > 3:
-    print("Erro: severidade inválida!!")
-    exit()
-
-#Horas
-try:
-    quantidade_de_horas = int(input("Digíte a quantas horas o chamado está em aberto: "))
-except ValueError:
-    print("Erro: você deve digitar um número!!")
-if quantidade_de_horas < 0:
-    print("Erro: Horas não podem ser negativas!!")
-
-#Usuários afetados
-try:
-    usuarios_afetados = int(input("Digíte a quantidade de usuários afetados pelo prbloema: "))
-except ValueError:
-    print("Erro: você deve digitar um número!!")
-if usuarios_afetados < 0:
-    print("Erro: número de usuários inválido!!")
-    exit()
-
-#Ambiente
-    ambiente = input("Digite o ambiente (produção ou teste): ").lower()
-if ambiente not in ["produção", "teste"]:
-    print("Erro: ambiente inválido ")
-    exit()
-
-
-#Validação dos dados
-if severidade_do_problema < 1 or severidade_do_problema > 3:
-    print("Erro: severidade inválida!!")
-
+severidade_do_problema = int(input("Dígite a severidade do problema (1 a 3): "))
+quantidade_de_horas = int(input("Digíte a quantas horas o chamado está em aberto: "))
+usuarios_afetados = int(input("Digíte a quantidade de usuários afetados pelo prbloema: "))
+ambiente = input("Digite o ambiente (produção ou teste): ").lower()
 
 #Classificação de prioridades.
 if severidade_do_problema == 3 and ambiente == "produção":
     prioridade = "Crítica"
-elif severidade_do_problema == 3 and usuarios_afetados > 100:
+elif severidade_do_problema == 3 or usuarios_afetados > 100:
     prioridade = "Alta"
-elif severidade_do_problema == 2 and quantidade_de_horas > 4:
+elif severidade_do_problema == 2 or quantidade_de_horas > 4:
     prioridade = "Média"
 else:
     prioridade = "Baixa"
