bd120141ddb78567587f5125fd2afaa3fb0559f9
Rikk <tarikmoussa90@gmial.com>
2026-03-25 15:54:27 -0300

    Começando mini projeto de PyThon e inicialização do repoitório.

---
+++ sistema_prioridade.py
@@ -0,0 +1,11 @@
+#O mini projeto foi feito por:
+#Tárik Moussa Alma - RM: 571411
+#Fábricio Aquiles - RM: 570985
+#Giovanne Dias - RM: 569750
+
+#Entrada de dados
+severidade_do_problema = int(input("Dígite a severidade do problema (1 a 3): "))
+quantidade_de_horas = int(input("Digíte a quantas horas o chamado está em aberto: "))
+usuarios_afetados = int(input("Digíte a quantidade de usuários afetados pelo prbloema: "))
+ambiente = input("Digite o ambiente (produção ou teste): ").lower()
+
