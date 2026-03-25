79e6b70e52ed007d4aa412dd48109b6faf44ae7b
Rikk <tarikmoussa90@gmial.com>
2026-03-25 16:11:43 -0300

    Alterando a saída de dados

--- sistema_prioridade.py
+++ sistema_prioridade.py
@@ -3,13 +3,13 @@
 #Fábricio Aquiles - RM: 570985
 #Giovanne Dias - RM: 569750
 
-#Entrada de dados
+#Entrada de dados.
 severidade_do_problema = int(input("Dígite a severidade do problema (1 a 3): "))
 quantidade_de_horas = int(input("Digíte a quantas horas o chamado está em aberto: "))
 usuarios_afetados = int(input("Digíte a quantidade de usuários afetados pelo prbloema: "))
 ambiente = input("Digite o ambiente (produção ou teste): ").lower()
 
-#Classificação de prioridades
+#Classificação de prioridades.
 if severidade_do_problema == 3 and ambiente == "produção":
     prioridade = "Prioridade Crítica"
 elif severidade_do_problema == 3 and usuarios_afetados > 100:
@@ -19,4 +19,14 @@ elif severidade_do_problema == 2 and quantidade_de_horas > 4:
 else:
     prioridade = "Prioridade Baixa"
 
+#O que fazer baseado na prioridade.
+if prioridade == "Crítica":
+    acao = "Acionar plantão imediatamente!!"
+elif prioridade == "Alta":
+    acao = "Iremos te encaminhar para a equipe responsável."
+elif prioridade == "Média":
+    acao = "Se regristre e acompanhe."
+else:
+    acao = "Colocar na fila de atendimento."
 
+#Saída de Dados.
